639d658e6bbcc29a4e81dbd319b5eeefae7a3713
IndexXuan <indexxuan@gmail.com>
2021-08-09 15:33:12 +0800

    fix: support url without quotation marks and code refactor

--- src/css-loader-compat.ts
+++ src/css-loader-compat.ts
@@ -3,8 +3,8 @@ import type { Plugin } from 'vite'
 const cssLangs = `\\.(css|less|sass|scss|styl|stylus|pcss|postcss)($|\\?)`
 const cssLangRE = new RegExp(cssLangs)
 const cssUrlRE = /(?:^|[^\w\-\u0080-\uffff])url\(\s*('[^']+'|"[^"]+"|[^'")]+)\s*\)/
-const vueLangs = /\.(vue)$|vue&type=template|vue&type=style/
-const publicRe = /(['|"])~?\/?public(\/)/g
+const vueLangRE = /\.(vue)$|vue&type=template|vue&type=style/
+const publicRE = /(['|"]?)~?\/?public(\/)/g
 
 /**
  * Plugin applied before user plugins
@@ -15,10 +15,18 @@ export default function cssLoaderCompat(): Plugin {
     enforce: 'pre',
 
     async transform(code, id) {
-      let ret = code
-      if (!cssLangRE.test(id) && !(vueLangs.test(id) && publicRe.test(code))) {
+      // not *.vue template or style, bypass
+      if (!vueLangRE.test(id)) {
         return
       }
+      // not css pre/post tools, bypass
+      // and not include public token, bypass
+      if (!cssLangRE.test(id) && !publicRE.test(code)) {
+        return
+      }
+
+      let ret = code
+      ret = ret.replace(publicRE, '$1$2')
       /**
        * @see {@link https://github.com/vuejs/vue-next/blob/ab6e927041e4082acac9a5effe332557e70e4f2a/packages/compiler-sfc/src/templateUtils.ts#L24}
        */
@@ -26,7 +34,6 @@ export default function cssLoaderCompat(): Plugin {
         return matchedUrl.replace('~', '')
       })
 
-      ret = ret.replace(publicRe, '$1$2')
       return ret
     },
   }
